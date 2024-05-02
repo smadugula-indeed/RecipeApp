baa4c4a4cb564e97d01266895bb1100192bcb974
Soumya Madugula <smadugula@indeed.com>
2024-05-02 12:48:18 +0530

    Fix local

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React, { useState } from "react";
 import "./App.css";
 import RecipeCreate from "./RecipeCreate";
 import RecipeList from "./RecipeList";
-import RecipeData from "./RecipeData"
+import RecipeData from "./RecipeData";
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
-  const addRecipes = (recipe) => {
-      setRecipes((oldRecipes) => [...oldRecipes, recipe]);
-  }
 
-  const handleDelete = (recipeIndex) => {
-      setRecipes((oldRecipes) => oldRecipes.filter((recipe, index) => index !== recipeIndex));
-  }
-  
+  const updateRecipes = (recipe) => {
+    setRecipes((oldRecipes) => [...oldRecipes, recipe]);
+  };
+
+  const onDelete = (recipeIndex) => {
+    setRecipes((oldRecipes) =>
+      oldRecipes.filter((recipe, index) => index !== recipeIndex)
+    );
+  };
+
   return (
     <div className="App">
-      <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList recipes={recipes} handleDelete={handleDelete} />
-      <RecipeCreate addRecipes={addRecipes} />
+      <header>
+        <h1>Delicious Food Recipes</h1>
+      </header>
+      <RecipeList recipes={recipes} handleDelete={onDelete} />
+      <RecipeCreate addRecipes={updateRecipes} />
     </div>
   );
 }
